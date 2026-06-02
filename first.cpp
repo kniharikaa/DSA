bb6840fcd4d3cd63b23b57f95a0568abf6f3395b
Niharika Khandelwal <kniharika652@gmail.com>
2026-06-02 23:15:20 +0530

    Fisrt Program

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+    if(age>18){
+        cout<<"yes\n";
+    }else{
+        cout<<"no\n";
+    }
+    return 0;
+}
