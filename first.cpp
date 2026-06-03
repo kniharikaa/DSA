01aa4d000f70cc6cf2ebe48f07fb46b6e3a4f4c1
Niharika Khandelwal <kniharika652@gmail.com>
2026-06-03 10:04:03 +0530

    trial program

--- first.cpp
+++ first.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int age;
-    cout<<"Enter your age: ";
-    cin>>age;
-    if(age>18){
-        cout<<"yes\n";
-    }else{
-        cout<<"no\n";
+//sum of all number from 1 to n divisible by 3
+int main(){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        if(i%3==0){
+            sum+=i;
+        }
     }
+    cout<<sum<<endl;
     return 0;
 }
