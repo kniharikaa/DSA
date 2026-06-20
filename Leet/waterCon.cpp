95b8244f7057c24e9476ebf17a3f2ffbd86c07d5
Niharika Khandelwal <kniharika652@gmail.com>
2026-06-20 12:55:37 +0530

    Leet 11

---
+++ Leet/waterCon.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int maxArea(vector<int> & height){
+    int maxWater=0;
+    int lp=0; int rp=height.size()-1;
+    while(lp<rp){
+        int w = rp-lp;
+        int ht = min(height[lp], height[rp]);
+        int currWater = w*ht;
+        maxWater= max(maxWater, currWater);  
+        height[lp]<height[rp] ? lp++ : rp--;
+    }
+    return maxWater;
+}
+int main(){
+    vector<int> height ={1,8,6,2,5,4,8,3,7};
+    cout<<maxArea(height)<<endl;
+    return 0;
+}
